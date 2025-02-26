eb782e6429957e3dcd80127fd9de8ec21df87c93
Karle <al217769@alumnos.uacj.mx>
2025-02-26 08:36:46 -0700

    [AGREGA] una basica de la interfaz con diseño sensillo

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Esta es una aplicacion de calculadora sencilla
 y debera cumplir con lo siguiente
 [ ] interfaz de maquina de estados para controlar el comportamiento
 [ ] Calcular almenos 4 operaciones basicas de una calculadora
+    [ ] Sumar
+    [ ] restar
+    [ ] 
+    [ ]
--- app/src/main/java/com/example/calculadora_a/MainActivity.kt
+++ app/src/main/java/com/example/calculadora_a/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculadora_a.ui.theme.Calculadora_aTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,22 +29,71 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Calculadora_aTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Calculada()
                 }
             }
         }
     }
-}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Calculada() {
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center) {
+        Text(text = "Numeritos", modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .background(Color.Blue)
+            .height(50.dp),
+            textAlign = TextAlign.Right,
+        )
+
+        Column (modifier = Modifier.fillMaxSize()
+            .background(Color.DarkGray)){
+            Row(horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()) {
+                Boton("7")
+                Boton("8")
+                Boton("9")
+
+            }
+            Row(horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()) {
+                Boton("4")
+                Boton("5")
+                Boton("6")
+
+            }
+            Row(horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()) {
+                Boton("1")
+                Boton("2")
+                Boton("3")
+
+            }
+            Row(horizontalArrangement = Arrangement.SpaceAround,
+                modifier = Modifier.fillMaxWidth()) {
+                Boton(".")
+                Boton("0")
+                Boton("op")
+
+            }
+
+        }
+    }
+
+}
+
+
+@Composable
+fun Boton(etiqueta: String){
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        etiqueta, modifier = Modifier
+            .padding(25.dp)
+            .background(Color.Green)
+            .height(50.dp)
+            .width(50.dp),
+        textAlign = TextAlign.Center,
+        //fontSize
     )
 }
 
@@ -42,6 +101,15 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Calculadora_aTheme {
-        Greeting("Android")
+        Calculada()
     }
+}
+
+@Preview
+@Composable
+fun  mostrar_btn() {
+    Calculadora_aTheme {
+        Boton("4")
+    }
+    
 }
